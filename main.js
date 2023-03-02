455b6a21270d408986ced64656ae2a5785cabe49
Showman_Das <Showmandas3216@gmail.com>
2023-03-02 21:48:59 +0600

    load api in console

---
+++ main.js
@@ -0,0 +1,10 @@
+const loadData=()=>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res=>res.json())
+    .then(data=>console.log(data.data))
+}
+
+
+
+
+// console.log(loadData());
